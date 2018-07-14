658367e8df866cfb7be7ffbe33b67376ee1d5e79
agnaldoae <agnaldo.campinas@gmail.com>
2018-07-14 12:27:20 -0300

    loading data and print a chart

---
+++ main.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import configparser  # in Python 2.x use 'configParser'
+
+
+if __name__ == "__main__":
+
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    DATA_DIR = config['PATHS']['DATA_DIR']
+    HEADER = int(config['DEFAULT']['HEADER'])
+    INDEX_COL  = int(config['DEFAULT']['INDEX_COL'])
+
+    #print(data_dir)
+    df = pd.read_csv(DATA_DIR, header=HEADER, index_col=INDEX_COL)
+ 
+    df['Close'].plot()
+    plt.title('AT&T share prices')
+    plt.xlabel('Date')
+    plt.ylabel('Price (US$)')
+    plt.legend()
+    plt.show()
+    
