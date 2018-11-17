85a46697b162a6fb1f9c21436ac9389ccac4cc3b
agnaldoae <agnaldo.campinas@gmail.com>
2018-11-17 08:11:01 -0200

    Update generatingRPs.py

--- miscellaneous/generatingRPs.py
+++ miscellaneous/generatingRPs.py
@@ -59,5 +59,5 @@ if __name__ == "__main__":
             rp = rplot(subseries)
             Mat2Image(rp,"./rps/"+labels[i]+"/"+labels[i]+"."+indexs[i]+"."+symbol+".jpg" )
 
-            symbol = input("Enter stock symbol or ! to stop: ")
+        symbol = input("Enter stock symbol or ! to stop: ")
 
